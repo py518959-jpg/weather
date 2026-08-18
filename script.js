6e7751b26270e1c56969958670250c80db2ddf0e
py518959-jpg <py518959@gmail.com>
2026-08-18 14:49:57 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+let cloud = document.querySelector(".cloudy");
+let sunny = document.querySelector(".sunny");
+let ranny = document.querySelector(".ranny");
+let thunder = document.querySelector(".thunder");
+
