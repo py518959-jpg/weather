1c4c13bcc67da55908f91ad0f6c33effdb85e336
py518959-jpg <py518959@gmail.com>
2026-08-20 17:40:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,41 +7,48 @@ let hamburger = document.querySelector(".hamburger");
 let middlefinger = document.querySelector(".middlefinger");
 let hhj = document.querySelector(".hhj");
 
-let input = document.querySelector(".w_input");
+let w_input = document.querySelector(".w_input");
 
-input.addEventListener("input", () => {
+
+w_input.addEventListener("input", () => {
     cloud.classList.remove("on");
     sunny.classList.remove("on");
-    ranny.classList.remove("on");
+    rainy.classList.remove("on");
     thunder.classList.remove("on");
     snowy.classList.remove("on");
     hamburger.classList.remove("on");
     middlefinger.classList.remove("on");
     hhj.classList.remove("on");
 
-    if (input.value == "흐림" || input.value == "구름" || input.value == "구름2" || input.value == "구름이") {
+    if (w_input.value == "흐림" || w_input.value == "구름" || w_input.value == "구름2" || w_input.value == "구름이") {
         cloud.classList.add("on");
     }
-    else if (input.value == "맑음") {
+    else if (w_input.value == "맑음") {
         sunny.classList.add("on");
     }
-    else if (input.value == "비" || input.value == "강우") {
+    else if (w_input.value == "비" || w_input.value == "강우") {
         rainy.classList.add("on");
     }
-    else if (input.value == "번개" || input.value == "뇌우") {
+    else if (w_input.value == "번개" || w_input.value == "뇌우") {
         thunder.classList.add("on");
     }
-    else if (input.value == "눈") {
+    else if (w_input.value == "눈") {
         snowy.classList.add("on");
     }
-    else if (input.value == "햄버거") {
+    else if (w_input.value == "햄버거") {
         hamburger.classList.add("on");
     }
-    else if (input.value == "중지") {
+    else if (w_input.value == "중지") {
         middlefinger.classList.add("on");
     }
-    else if (input.value == "한형준" || input.value == "형준" || input.value == "멍청이") {
+    else if (w_input.value == "한형준" || w_input.value == "형준" || w_input.value == "멍청이") {
         hhj.classList.add("on");
     }
 });
 
+let t_input = document.querySelector(".t_input");
+let temperature = document.querySelector(".temperature_s");
+
+t_input.addEventListener("input", () => {
+    temperature.textContent = t_input.value;
+})
